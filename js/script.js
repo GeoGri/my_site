ae2c2a13bcc2f7065d567f221a099add0ba15ebd
Gregory Wojda <grzegorz1wojda@gmail.com>
2018-10-09 10:50:32 +0200

    First section

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	parallaxEffect();
+
+
+//Paralax effect with mouse movement
+function parallaxEffect(){
+	var scene = $('#scene').get(0);
+	var parallaxInstance = new Parallax(scene);
+};
